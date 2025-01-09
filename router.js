21b44070c26bc0e7f825e0c8c38f87ca3f65b4e0
syed sheheen <you@example.com>
2025-01-09 17:32:04 +0530

    d

--- router.js
+++ router.js
@@ -5,10 +5,10 @@ const express = require('express')
 const userController = require('./controller/userController')
 
 //import projectController
-const projectController  = require('./controller/projectController')
+const projectController = require('./controller/projectController')
 
 //import jwtMiddleware
-const jwtMiddleware=require('./middleware/jwtMiddleware')
+const jwtMiddleware = require('./middleware/jwtMiddleware')
 
 //import multer
 const multerConfig = require('./middleware/multerMiddleware')
@@ -17,12 +17,21 @@ const multerConfig = require('./middleware/multerMiddleware')
 const router = new express.Router()
 
 // register
- router.post('/register',userController.register)
+router.post('/register', userController.register)
 
- // login
- router.post('/login',userController.login)
+// login
+router.post('/login', userController.login)
 
 // add project
-router.post('/add-project',jwtMiddleware,multerConfig.single("projectImage"),projectController.addProjectController)
+router.post('/add-project', jwtMiddleware, multerConfig.single("projectImage"), projectController.addProjectController)
+
+// Get all projects
+router.get('/all-project', projectController.getAllProjectController)
+
+// Get home projects
+router.get('/home-project', projectController.getHomeProjectController)
+
+// Get user project
+router.get('/user-project', jwtMiddleware,projectController.getUserProjectController)
 
 module.exports = router
