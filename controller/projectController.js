bcae5b65c380d65151988d222cb29efb288c4f0c
syed sheheen <you@example.com>
2025-01-09 16:30:53 +0530

    d

--- controller/projectController.js
+++ controller/projectController.js
@@ -28,3 +28,34 @@ exports.addProjectController = async (req, res) => {
     }
 
 }
+
+// get all projects
+exports.getAllProjectController=async(req,res)=>{
+try {
+    const allProject = await projects.find()
+    res.status(200).json(allProject) 
+} catch (error) {
+    res.status(401).json(error)
+}
+}
+
+// get home projects
+exports.getHomeProjectController=async(req,res)=>{
+    try {
+        const allProject = await projects.find().limit(3)
+        res.status(200).json(allProject) 
+    } catch (error) {
+        res.status(401).json(error)
+    }
+    }
+
+// get user projects
+exports.getUserProjectController=async(req,res)=>{
+    const userId = req.payload
+    try {
+        const allProject = await projects.find({userId})
+        res.status(200).json(allProject) 
+    } catch (error) {
+        res.status(401).json(error)
+    }
+    }
